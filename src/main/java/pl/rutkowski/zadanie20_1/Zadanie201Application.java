fcd90542f77973ef9b859276fe48959668f1a67d
Adrian Rutkowski <adrianrutkowskiarcgas@gmail.com>
2023-01-06 09:25:51 +0100

    Stworzenie projektu

---
+++ src/main/java/pl/rutkowski/zadanie20_1/Zadanie201Application.java
@@ -0,0 +1,13 @@
+package pl.rutkowski.zadanie20_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie201Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rutkowski/zadanie20_1/Zadanie201ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rutkowski.zadanie20_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
