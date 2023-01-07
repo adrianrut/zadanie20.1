04ff80db636c8be8b0d095fdde8aef7045a4cd7a
Adrian Rutkowski <adrianrutkowskiarcgas@gmail.com>
2023-01-07 12:43:41 +0100

    Rozwiazanie

---
+++ src/main/java/pl/rutkowski/zadanie20_1/UserList.java
@@ -0,0 +1,21 @@
+package pl.rutkowski.zadanie20_1;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserList {
+    List<User> userList = new ArrayList<>();
+
+    public UserList() {
+        userList.add(new User("Jan", "Kowalski", 30));
+        userList.add(new User("Andrzej", "Nowak", 45));
+        userList.add(new User("Zenon", "Kwiatkowski", 30));
+    }
+
+    public List<User> getAll() {
+        return userList;
+    }
+}
